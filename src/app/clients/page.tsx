915806a847fc5dca3b045effb2d6da7771023486
LifeAsDev <angelosarmiento5d@gmail.com>
2024-08-15 18:01:59 -0400

    clients table

---
+++ src/app/clients/page.tsx
@@ -0,0 +1,5 @@
+import Clients from "@/components/clients/clients";
+
+export default function Page() {
+  return <Clients />;
+}
---
+++ src/models/client.ts
@@ -0,0 +1,11 @@
+interface Client {
+  fullName: string;
+  numero: string;
+  correo: string;
+  direccion: string;
+  notas: string;
+  id: string;
+  _id: string;
+}
+
+export default Client;

a45c5b5ab0f226f43d92027c1e565c2a68f43b94
LifeAsDev <angelosarmiento5d@gmail.com>
2024-09-12 15:58:55 -0400

    order form set client function

--- src/components/orders/createOrder/setOrderClientForm/setOrderClientForm.tsx
+++ src/components/orders/createOrder/setOrderClientForm/setOrderClientForm.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import styles from "./styles.module.css";
 
 export default function SetOrderClientForm() {
-  const [isHidden, setIsHidden] = useState(false); // Estado para controlar la visibilidad
+  const [isHidden, setIsHidden] = useState(true); // Estado para controlar la visibilidad
   const collapsibleRef = useRef<HTMLDivElement>(null);
   const [clientData, setClientData] = useState<Client>({
     fullName: "",
@@ -14,6 +14,7 @@ export default function SetOrderClientForm() {
     id: "",
     notas: "",
   });
+  const [clientSelected, setClientSelected] = useState(false);
   const [clientErrors, setClientErrors] = useState<
     Partial<Record<keyof Client, string>>
   >({});
@@ -40,9 +41,7 @@ export default function SetOrderClientForm() {
     const container = document.getElementById("setOrderClientForm");
     if (container) {
       // Si está oculto, colapsa la altura a 0, si no, ajusta la altura a "auto"
-      container.style.height = isHidden
-        ? "200px"
-        : `${container.scrollHeight}px`;
+      container.style.height = isHidden ? "0" : `${container.scrollHeight}px`;
     }
   }, [isHidden]);
   useEffect(() => {
@@ -71,61 +70,124 @@ export default function SetOrderClientForm() {
     fetchClients();
   }, [clientData.fullName]);
 
+  const handleSetClient = (item: Client) => {
+    setIsHidden(true);
+    setClientData(item);
+    setClientSelected(true);
+  };
+  const handleClearClient = () => {
+    setClientSelected(false);
+    setClientData({
+      fullName: "",
+      numero: "",
+      correo: "",
+      direccion: "",
+      id: "",
+      notas: "",
+    });
+  };
   return (
-    <div id="setOrderClientForm" className={styles.setOrderClientForm}>
+    <div className={styles.setOrderClientForm}>
       <h3>Cliente</h3>
-      <div className={styles.inputGroup}>
-        <label htmlFor="fullName" className={styles.label}>
-          Nombre completo:
-        </label>
-        <input
-          disabled={creatingOrder}
-          type="text"
-          id="fullName"
-          name="fullName"
-          value={clientData.fullName}
-          onChange={handleChange}
-          className={`${styles.input} ${
-            clientErrors.fullName ? styles.errorInput : ""
-          }`}
-          onFocus={() => {
-            setClientErrors({});
-          }}
-        />
-        {clientErrors.fullName && (
-          <p className={styles.errorText}>{clientErrors.fullName}</p>
-        )}
-        <ul className={styles.clientsList}>
-          {fetchingMonitor ? (
-            <li className={styles.infoSearchFetch}>Buscando...</li>
-          ) : (
-            <>
-              {clientsArr.length > 0 ? (
-                <>
-                  {clientsArr.map((item) => {
-                    return (
-                      <li
-                        onMouseDown={() => {
-                          setClientData(item);
-                        }}
-                        key={item._id}
-                      >
-                        {item.fullName} {item.id}
-                      </li>
-                    );
-                  })}
-                </>
-              ) : (
-                <li className={styles.infoSearchFetch}>
-                  No se encontraron clientes.
-                </li>
-              )}
-            </>
+      {clientSelected ? (
+        <>
+          <ul className={styles.clientSelectedUl}>
+            <li>
+              <p className={styles.clientLabel}>Nombre</p>
+              <p className={styles.clientData}>{clientData.fullName}</p>
+            </li>
+            <li>
+              <p className={styles.clientLabel}>RUT</p>
+              <p className={styles.clientData}>{clientData.id}</p>
+            </li>
+            <li>
+              <p className={styles.clientLabel}>Correo</p>
+              <p className={styles.clientData}>{clientData.correo}</p>
+            </li>
+            <li>
+              <p className={styles.clientLabel}>Direccion</p>
+              <p className={styles.clientData}>{clientData.direccion}</p>
+            </li>
+          </ul>
+          <svg
+            onClick={handleClearClient}
+            className={styles.closeBtn}
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="var(--light-grayish-blue)"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fill="var(--light-grayish-blue)"
+                d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z"
+              ></path>
+            </g>
+          </svg>
+        </>
+      ) : (
+        <div className={styles.inputGroup}>
+          <label htmlFor="fullName" className={styles.label}>
+            Nombre completo:
+          </label>
+          <input
+            disabled={creatingOrder}
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={clientData.fullName}
+            onChange={handleChange}
+            className={`${styles.input} ${
+              clientErrors.fullName ? styles.errorInput : ""
+            }`}
+            onFocus={() => {
+              setClientErrors({});
+            }}
+          />
+          {clientErrors.fullName && (
+            <p className={styles.errorText}>{clientErrors.fullName}</p>
           )}
-        </ul>
-      </div>
+          <ul className={styles.clientsList}>
+            {fetchingMonitor ? (
+              <li className={styles.infoSearchFetch}>Buscando...</li>
+            ) : (
+              <>
+                {clientsArr.length > 0 ? (
+                  <>
+                    {clientsArr.map((item) => {
+                      return (
+                        <li
+                          onMouseDown={() => {
+                            handleSetClient(item);
+                          }}
+                          key={item._id}
+                        >
+                          {item.fullName}
+                          <span className={styles.clientId}>{item.id}</span>
+                        </li>
+                      );
+                    })}
+                  </>
+                ) : (
+                  <li className={styles.infoSearchFetch}>
+                    No se encontraron clientes.
+                  </li>
+                )}
+              </>
+            )}
+          </ul>
+        </div>
+      )}
       <p
-        onClick={() => setIsHidden(!isHidden)}
+        onClick={() => {
+          setIsHidden(!isHidden);
+          setClientSelected(false);
+        }}
         className={styles.toggleClientFields}
       >
         {isHidden ? "Crear nuevo cliente" : "Ocultar"}
@@ -152,113 +214,117 @@ export default function SetOrderClientForm() {
           </g>
         </svg>
       </p>
-      <div className={styles.inputGroup}>
-        <label htmlFor="id" className={styles.label}>
-          ID:
-        </label>
-        <input
+      <div id="setOrderClientForm" className={styles.collapsibleContent}>
+        <div className={styles.inputGroup}>
+          <label htmlFor="id" className={styles.label}>
+            ID:
+          </label>
+          <input
+            disabled={creatingOrder}
+            type="text"
+            id="id"
+            name="id"
+            value={clientData.id}
+            onChange={handleChange}
+            className={`${styles.input} ${
+              clientErrors.id ? styles.errorInput : ""
+            }`}
+            onFocus={() => setClientErrors({})}
+          />
+          {clientErrors.id && (
+            <p className={styles.errorText}>{clientErrors.id}</p>
+          )}
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="numero" className={styles.label}>
+            Número:
+          </label>
+          <input
+            disabled={creatingOrder}
+            type="text"
+            id="numero"
+            name="numero"
+            value={clientData.numero}
+            onChange={handleChange}
+            className={`${styles.input} ${
+              clientErrors.numero ? styles.errorInput : ""
+            }`}
+            onFocus={() => setClientErrors({})}
+          />
+          {clientErrors.numero && (
+            <p className={styles.errorText}>{clientErrors.numero}</p>
+          )}
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="correo" className={styles.label}>
+            Correo:
+          </label>
+          <input
+            disabled={creatingOrder}
+            type="text"
+            id="correo"
+            name="correo"
+            value={clientData.correo}
+            onChange={handleChange}
+            className={`${styles.input} ${
+              clientErrors.correo ? styles.errorInput : ""
+            }`}
+            onFocus={() => setClientErrors({})}
+          />
+          {clientErrors.correo && (
+            <p className={styles.errorText}>{clientErrors.correo}</p>
+          )}
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="direccion" className={styles.label}>
+            Dirección:
+          </label>
+          <input
+            disabled={creatingOrder}
+            type="text"
+            id="direccion"
+            name="direccion"
+            value={clientData.direccion}
+            onChange={handleChange}
+            className={`${styles.input} ${
+              clientErrors.direccion ? styles.errorInput : ""
+            }`}
+            onFocus={() => setClientErrors({})}
+          />
+          {clientErrors.direccion && (
+            <p className={styles.errorText}>{clientErrors.direccion}</p>
+          )}
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="notas" className={styles.label}>
+            Notas:
+          </label>
+          <input
+            disabled={creatingOrder}
+            type="text"
+            id="notas"
+            name="notas"
+            value={clientData.notas}
+            onChange={handleChange}
+            className={`${styles.input} ${
+              clientErrors.notas ? styles.errorInput : ""
+            }`}
+            onFocus={() => setClientErrors({})}
+          />
+          {clientErrors.notas && (
+            <p className={styles.errorText}>{clientErrors.notas}</p>
+          )}
+        </div>
+        <button
           disabled={creatingOrder}
-          type="text"
-          id="id"
-          name="id"
-          value={clientData.id}
-          onChange={handleChange}
-          className={`${styles.input} ${
-            clientErrors.id ? styles.errorInput : ""
+          onClick={() => {}}
+          className={`${styles.button} ${
+            creatingOrder && styles.creatingClient
           }`}
-          onFocus={() => setClientErrors({})}
-        />
-        {clientErrors.id && (
-          <p className={styles.errorText}>{clientErrors.id}</p>
-        )}
+        >
+          {!creatingOrder ? "Crear" : <div className="loader"></div>}
+        </button>
       </div>
-      <div className={styles.inputGroup}>
-        <label htmlFor="numero" className={styles.label}>
-          Número:
-        </label>
-        <input
-          disabled={creatingOrder}
-          type="text"
-          id="numero"
-          name="numero"
-          value={clientData.numero}
-          onChange={handleChange}
-          className={`${styles.input} ${
-            clientErrors.numero ? styles.errorInput : ""
-          }`}
-          onFocus={() => setClientErrors({})}
-        />
-        {clientErrors.numero && (
-          <p className={styles.errorText}>{clientErrors.numero}</p>
-        )}
-      </div>
-      <div className={styles.inputGroup}>
-        <label htmlFor="correo" className={styles.label}>
-          Correo:
-        </label>
-        <input
-          disabled={creatingOrder}
-          type="text"
-          id="correo"
-          name="correo"
-          value={clientData.correo}
-          onChange={handleChange}
-          className={`${styles.input} ${
-            clientErrors.correo ? styles.errorInput : ""
-          }`}
-          onFocus={() => setClientErrors({})}
-        />
-        {clientErrors.correo && (
-          <p className={styles.errorText}>{clientErrors.correo}</p>
-        )}
-      </div>
-      <div className={styles.inputGroup}>
-        <label htmlFor="direccion" className={styles.label}>
-          Dirección:
-        </label>
-        <input
-          disabled={creatingOrder}
-          type="text"
-          id="direccion"
-          name="direccion"
-          value={clientData.direccion}
-          onChange={handleChange}
-          className={`${styles.input} ${
-            clientErrors.direccion ? styles.errorInput : ""
-          }`}
-          onFocus={() => setClientErrors({})}
-        />
-        {clientErrors.direccion && (
-          <p className={styles.errorText}>{clientErrors.direccion}</p>
-        )}
-      </div>
-      <div className={styles.inputGroup}>
-        <label htmlFor="notas" className={styles.label}>
-          Notas:
-        </label>
-        <input
-          disabled={creatingOrder}
-          type="text"
-          id="notas"
-          name="notas"
-          value={clientData.notas}
-          onChange={handleChange}
-          className={`${styles.input} ${
-            clientErrors.notas ? styles.errorInput : ""
-          }`}
-          onFocus={() => setClientErrors({})}
-        />
-        {clientErrors.notas && (
-          <p className={styles.errorText}>{clientErrors.notas}</p>
-        )}
-      </div>
-      <button
-        disabled={creatingOrder}
-        onClick={() => {}}
-        className={`${styles.button} ${creatingOrder && styles.creatingClient}`}
-      >
-        {!creatingOrder ? "Crear" : <div className="loader"></div>}
-      </button>
     </div>
   );
 }

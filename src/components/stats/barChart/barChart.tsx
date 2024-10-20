9a6d93ab11429bf932c406982ab2cf53f1b15fe9
LifeAsDev <angelosarmiento5d@gmail.com>
2024-10-19 20:24:52 -0400

    chart initial

---
+++ src/components/stats/barChart/barChart.tsx
@@ -0,0 +1,77 @@
+import { useRef, useEffect } from "react";
+import Chart from "chart.js/auto";
+import styles from "./styles.module.css";
+const BarChart = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const chartRef = useRef<Chart | null>(null); // Ref para mantener la instancia del gráfico
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d");
+
+      if (chartRef.current) {
+        chartRef.current.destroy(); // Destruir el gráfico existente antes de crear uno nuevo
+      }
+
+      if (ctx) {
+        chartRef.current = new Chart(ctx, {
+          type: "bar",
+          data: {
+            labels: [
+              "Enero",
+              "Febrero",
+              "Marzo",
+              "Abril",
+              "Mayo",
+              "Junio",
+              "Julio",
+              "Agosto",
+              "Septiembre",
+              "Octubre",
+              "Noviembre",
+              "Diciembre",
+            ],
+            datasets: [
+              {
+                label: "Ganancias (CLP)",
+                data: [
+                  500000, 700000, 300000, 800000, 1000000, 1500000, 1000000,
+                  900000, 1100000, 1300000, 1700000, 1400000,
+                ], // Datos de ganancias por mes en pesos chilenos
+                backgroundColor: "rgba(75, 192, 192, 0.2)", // Color de las barras
+                borderColor: "rgba(75, 192, 192, 1)", // Color del borde de las barras
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+                ticks: {
+                  callback: function (value) {
+                    return "$" + value.toLocaleString("es-CL"); // Formato de pesos chilenos
+                  },
+                },
+              },
+            },
+          },
+        });
+      }
+    }
+
+    // Limpiar al desmontar el componente
+    return () => {
+      if (chartRef.current) {
+        chartRef.current.destroy();
+      }
+    };
+  }, []);
+  return (
+    <div>
+      <canvas className={styles.canvas} ref={canvasRef}></canvas>
+    </div>
+  );
+};
+
+export default BarChart;
--- src/components/stats/stats.tsx
+++ src/components/stats/stats.tsx
@@ -1,4 +1,11 @@
+"use client";
+import BarChart from "@/components/stats/barChart/barChart";
 import styles from "./styles.module.css";
 export default function Stats() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <h1>Grafica</h1>
+      <BarChart />
+    </main>
+  );
 }
